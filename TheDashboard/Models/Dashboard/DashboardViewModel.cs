38c70dd1cf2107c9f2cfe7bd7d0b43feb6e8f7c8
s0ba <s0ba@icloud.com>
2017-10-23 16:31:16 +0100

    Add Recycle Bin List

--- TheDashboard/Models/Dashboard/DashboardViewModel.cs
+++ TheDashboard/Models/Dashboard/DashboardViewModel.cs
@@ -9,17 +9,20 @@ namespace TheDashboard.Models
             Activities= new List<ActivityViewModel>();
             UnpublishedContent = new List<ActivityViewModel>();
             UserRecentActivity = new List<ActivityViewModel>();
+            ItemsInRecycleBin = new List<ActivityViewModel>();
         }
 
         public List<ActivityViewModel> Activities { get; set; }
         public List<ActivityViewModel> UnpublishedContent { get; set; }
         public List<ActivityViewModel> UserRecentActivity { get; set; }
 
+        public List<ActivityViewModel> ItemsInRecycleBin { get; set; }
+
+
         public int CountPublishedNodes { get; set; }
         public int CountContentInRecycleBin { get; set; }
         public int CountTotalWebsiteMembers { get; set; }
         public int CountNewMembersLastWeek { get; set; }
 
-        public List<ActivityViewModel> GetItemsInRecycleBin { get; set; }
     }
 }
